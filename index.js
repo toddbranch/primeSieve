645ee0c2dce22d9a077dbf87252de2d9f050f618
Todd Branchflower <todd.branchflower@gmail.com>
2012-12-06 14:24:15 -0700

    removed unnecessary line - returning early if requesting primes less than 2

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ function sieve(number) {
   result.push(0)
   for (var i = 0; i<=number-2; i++) result.push(1)
 
-  if (number<2) return result
-
   for (var i = 2; i<Math.ceil(Math.sqrt(number)); i++) {
     if (result[i]) {
       for (var j = 2*i; j <= number; j += i) {
         result[j] = 0
       }
     }
-
   }
   return result
 }
